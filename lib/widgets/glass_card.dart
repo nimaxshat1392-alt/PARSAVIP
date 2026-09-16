f1591602cd4a269619b643a6be5f4851f1fbe907
nimaxshat1392-alt <nimaxshat1392@gmail.com>
2026-09-16 14:07:36 +0330

    Create glass_card.dart

---
+++ lib/widgets/glass_card.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import '../theme/app_theme.dart';
+
+class GlassCard extends StatelessWidget {
+  final Widget child;
+  final EdgeInsets padding;
+  final VoidCallback? onTap;
+  final double radius;
+
+  const GlassCard({
+    super.key,
+    required this.child,
+    this.padding = const EdgeInsets.all(12),
+    this.onTap,
+    this.radius = 18,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 14, sigmaY: 14),
+          child: Container(
+            padding: padding,
+            decoration: BoxDecoration(
+              color: C.bgCard.withOpacity(0.55),
+              borderRadius: BorderRadius.circular(radius),
+              border: Border.all(color: C.border),
+            ),
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
