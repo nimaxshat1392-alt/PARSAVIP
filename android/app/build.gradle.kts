96f965c178e775973a04bbc3f5eab22bc182923f
nimaxshat1392-alt <nimaxshat1392@gmail.com>
2026-09-17 02:05:44 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,46 +1,46 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
-    id "dev.flutter.flutter-gradle-plugin"
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace "com.parsavip.parsavip"
-    compileSdk 35 // طبق نیازمندی پلاگین جدید
-    ndkVersion flutter.ndkVersion
+    namespace = "com.parsavip.parsavip"
+    compileSdk = 35
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = "17"
     }
 
     defaultConfig {
-        applicationId "com.parsavip.parsavip"
-        minSdkVersion 23 // حداقل نسخه مورد نیاز پلاگین
-        targetSdkVersion 34
-        versionCode 1
-        versionName "1.0.0"
+        applicationId = "com.parsavip.parsavip"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
     }
 
     buildTypes {
         release {
-            signingConfig signingConfigs.debug
-            minifyEnabled false
-            shrinkResources false
+            signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 
     packagingOptions {
         jniLibs {
-            useLegacyPackaging = true // طبق مستندات پلاگین برای لود صحیح کتابخانه‌های بومی
+            useLegacyPackaging = true
         }
     }
 }
 
 flutter {
-    source '../..'
+    source = "../.."
 }
