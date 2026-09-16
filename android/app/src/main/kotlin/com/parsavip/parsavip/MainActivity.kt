5dc959feeea0875fd3b5fdde0fe61951084b48d5
nimaxshat1392-alt <nimaxshat1392@gmail.com>
2026-09-17 01:19:13 +0330

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/parsavip/parsavip/MainActivity.kt
+++ android/app/src/main/kotlin/com/parsavip/parsavip/MainActivity.kt
@@ -2,7 +2,7 @@ package com.parsavip.parsavip
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import com.github.blueboytm.flutter_v2ray.v2ray.V2rayController
+import com.github.blueboytm.flutter_v2ray_client.v2ray.V2rayController
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
