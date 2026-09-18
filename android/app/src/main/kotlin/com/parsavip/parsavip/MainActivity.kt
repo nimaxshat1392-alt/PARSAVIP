0bfd089dea1a759a6ecce579c2d0b59141975de0
nimaxshat1392-alt <nimaxshat1392@gmail.com>
2026-09-18 16:05:12 +0330

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/parsavip/parsavip/MainActivity.kt
+++ android/app/src/main/kotlin/com/parsavip/parsavip/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.parsavip.parsavip
 
+import com.github.blueboytm.flutter_vless.FlutterVlessPlugin
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        FlutterVlessPlugin.setInstance(this)
+    }
+}
