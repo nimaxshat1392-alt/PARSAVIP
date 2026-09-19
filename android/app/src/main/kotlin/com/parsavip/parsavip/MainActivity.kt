d6c1ba0259c6cde8f68b97c66d019aea7695e94e
nimaxshat1392-alt <nimaxshat1392@gmail.com>
2026-09-19 19:07:01 +0330

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/parsavip/parsavip/MainActivity.kt
+++ android/app/src/main/kotlin/com/parsavip/parsavip/MainActivity.kt
@@ -2,13 +2,14 @@ package com.parsavip.parsavip
 
 import android.content.Intent
 import android.net.VpnService
-import com.v2ray.ang.service.V2RayVpnService
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
+    private val TAG = "MainActivity"
     private val METHOD_CHANNEL = "com.parsavip.parsavip/xray"
     private val EVENT_CHANNEL = "com.parsavip.parsavip/xray_events"
     private val VPN_PERMISSION_CODE = 0x0F01
@@ -23,7 +24,7 @@ class MainActivity : FlutterActivity() {
                     "start" -> {
                         val uri = call.argument<String>("config")
                         if (uri.isNullOrEmpty()) {
-                            result.error("INVALID", "Config URI empty", null)
+                            result.error("INVALID", "Config empty", null)
                             return@setMethodCallHandler
                         }
                         pendingConfig = uri
@@ -37,13 +38,19 @@ class MainActivity : FlutterActivity() {
                         }
                     }
                     "stop" -> {
-                        val i = Intent(this, V2RayVpnService::class.java)
-                        i.action = V2RayVpnService.ACTION_STOP
-                        startService(i)
-                        result.success(true)
+                        try {
+                            val i = Intent()
+                            i.setClassName(packageName, "com.v2ray.ang.service.V2RayVpnService")
+                            i.action = "com.v2ray.ang.action.STOP"
+                            startService(i)
+                            result.success(true)
+                        } catch (e: Exception) {
+                            Log.e(TAG, "stop error", e)
+                            result.error("STOP_FAIL", e.message, null)
+                        }
                     }
                     "status" -> {
-                        result.success(mapOf("connected" to V2RayVpnService.isRunning))
+                        result.success(mapOf("connected" to false))
                     }
                     else -> result.notImplemented()
                 }
@@ -52,26 +59,32 @@ class MainActivity : FlutterActivity() {
         EventChannel(flutterEngine.dartExecutor.binaryMessenger, EVENT_CHANNEL)
             .setStreamHandler(object : EventChannel.StreamHandler {
                 override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-                    V2RayVpnService.eventSink = events
+                    Log.i(TAG, "Event channel listening")
                 }
                 override fun onCancel(arguments: Any?) {
-                    V2RayVpnService.eventSink = null
+                    Log.i(TAG, "Event channel cancelled")
                 }
             })
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == VPN_PERMISSION_CODE && resultCode == RESULT_OK) {
+    override fun onActivityResult(requestCode: Int, requestCode2: Int, data: Intent?) {
+        super.onActivityResult(requestCode, requestCode2, data)
+        if (requestCode == VPN_PERMISSION_CODE && requestCode2 == RESULT_OK) {
             pendingConfig?.let { startV2Ray(it) }
         }
         pendingConfig = null
     }
 
     private fun startV2Ray(uri: String) {
-        val i = Intent(this, V2RayVpnService::class.java)
-        i.action = V2RayVpnService.ACTION_START
-        i.putExtra("config", uri)
-        startService(i)
+        try {
+            val i = Intent()
+            i.setClassName(packageName, "com.v2ray.ang.service.V2RayVpnService")
+            i.action = "com.v2ray.ang.action.START"
+            i.putExtra("config", uri)
+            startService(i)
+            Log.i(TAG, "V2Ray service started")
+        } catch (e: Exception) {
+            Log.e(TAG, "start error", e)
+        }
     }
 }
