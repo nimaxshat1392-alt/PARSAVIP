334eda3a0caac446af89ab6e605d497e219f5d1a
nimaxshat1392-alt <nimaxshat1392@gmail.com>
2026-09-21 00:17:50 +0330

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,21 +6,16 @@ plugins {
 
 android {
     namespace = "com.parsavip.parsavip"
-    compileSdk = 35
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
         jvmTarget = "17"
-    }
-
-    buildFeatures {
-        buildConfig = true
     }
 
     defaultConfig {
@@ -30,10 +25,6 @@ android {
         versionCode = 1
         versionName = "1.0.0"
         multiDexEnabled = true
-        buildConfigField("String", "APPLICATION_ID", "\"com.parsavip.parsavip\"")
-        buildConfigField("String", "VERSION_NAME", "\"1.0.0\"")
-        buildConfigField("int", "VERSION_CODE", "1")
-        buildConfigField("String", "PREFERENCE_NAME", "\"com.parsavip.parsavip_preferences\"")
     }
 
     buildTypes {
@@ -48,38 +39,7 @@ android {
         jniLibs {
             useLegacyPackaging = true
         }
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
-
-    sourceSets {
-        getByName("main") {
-            java.srcDirs("src/main/java", "src/main/kotlin")
-        }
-    }
-}
-
-dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar", "*.jar"))))
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.multidex:multidex:2.0.1")
-
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.tencent:mmkv:1.3.4")
-    implementation("com.jakewharton.timber:timber:5.0.1")
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:okhttp-urlconnection:4.12.0")
-    implementation("com.google.zxing:core:3.5.2")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
 }
 
 flutter {
